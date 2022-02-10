bcf4e55d89f95bbb337fa7fdf18612870b276842
Maurycy Z <10maurycy10@gmail.com>
2022-02-10 15:10:08 -0800

    minor fixes

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,18 @@
+#![forbid(unsafe_code)]
+
 fn lifetime_helper<'a, 'b, T>(_: &'a &'b (), v: &'b T) -> &'a T { v }
 
 fn lifetime_helper_mut<'a, 'b, T>(_: &'a &'b (), v: &'b mut T) -> &'a mut T { v }
 
 /// create a static refrence
+/// using https://github.com/rust-lang/rust/issues/25860
 pub fn remember<'a, T>(data: &'a T) -> &'static T {
     let f: fn(_, &'a T) -> &'static T = lifetime_helper;
     f(&&(), data)
 }
 
 /// change a mutable refrence's lifetime
+/// using https://github.com/rust-lang/rust/issues/25860
 pub fn remember_mut<'a, 'b, T>(data: &'a mut T) -> &'b mut T {
     let f: fn(_, &'a mut T) -> &'b mut T = lifetime_helper_mut;
     f(&&(), data)
