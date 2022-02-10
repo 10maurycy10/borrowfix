b97566d6ca5132adbf42426435fd6410d3b0fc47
Maurycy Z <10maurycy10@gmail.com>
2022-02-10 14:12:48 -0800

    init

---
+++ src/lib.rs
@@ -0,0 +1,27 @@
+pub fn remember<'a, T>(data: &'a T) -> &'static T {
+    unsafe {
+        std::mem::transmute(data)
+    }
+}
+
+pub fn remember_mut<'a, 'b, T>(data: &'a mut T) -> &'b mut T {
+    unsafe {
+        std::mem::transmute(data)
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use crate::remember;
+    #[test]
+    fn test_remember() {
+        let mut data = 1;
+        let refer = &data;
+        let refer = remember(refer);
+        let mut_refer = &mut data;
+        assert_eq!(*refer, 1);
+        *mut_refer = 2;
+        assert_eq!(*refer, 2);
+    }
+}
